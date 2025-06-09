dafa474a484b7e794a51a619e6bea97ed4950bdd
Avarinox <avarinox@gmail.com>
2025-06-09 23:18:03 +0200

    add cors

--- unsecured/backend/src/main.ts
+++ unsecured/backend/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:4200', 'http://192.168.137.1:4200'],
+    origin: "*",
     methods: 'GET,PUT,POST,DELETE, PATCH',
     allowedHeaders: 'Content-Type, Accept, Authorization',
     credentials: true,
